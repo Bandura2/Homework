f2227c9b637fd16fb1adb64b2bd7ff5a7891a0ba
Bandura2 <119165162+Bandura2@users.noreply.github.com>
2022-12-14 21:03:54 +0200

    changed lab_5

--- main.cpp
+++ main.cpp
@@ -1,64 +1,70 @@
 #include "Laba_5.h"
 
+/** Created a Sneaker class, added a SportShoesStore class
+ *  that contains an assortment of sneakers,
+ *  methods for sorting shoes by price,
+ *  and the ability to display the top most popular sneakers by number of sales */
+
 int main()
 {
     vector<Sneaker> sneakers;
-    cout <<"Enter number of objects:"<<endl;
-    int num_object;
-    cin >> num_object;
-    cout <<"----------------------------------------------------------"<< endl;
+    cout << "Enter number of objects: " << endl;
+    int count_of_object;
+    cin >> count_of_object;
+    cout << "----------------------------------------------------------" << endl;
 
-  for(int temporary_index = 0; temporary_index < num_object; temporary_index++)
+    for(int i = 0; i < count_of_object; i++)
     {
         sneakers.push_back(Sneaker());
     }
 
-    for(int temporary_index = 0; temporary_index < num_object; temporary_index++)
+    for(int i = 0; i < count_of_object; i++)
     {
-        cout << "\nSneaker "<< temporary_index + 1 <<" :"<<endl;
-        cin >> sneakers[temporary_index];
+        cout << "\nSneaker "<< i + 1 <<" :"<<endl;
+        cin >> sneakers[i];
     }
-    cout <<"----------------------------------------------------------"<< endl;
+    cout << "----------------------------------------------------------" << endl;
 
-    float *array_prices = new float[num_object];
-    for(int temporary_index = 0; temporary_index < num_object; temporary_index++)
+    float *array_prices = new float[count_of_object];
+    for(int i = 0; i < count_of_object; i++)
     {
-        array_prices[temporary_index] = sneakers[temporary_index].price_on_hryvna;
+        array_prices[i] = sneakers[i].price_on_hryvna;
     }
 
-    SportShoesStore object_for_sort;
+    SportShoesStore Object_for_sorting_array;
 
+    Object_for_sorting_array.Sort_array(array_prices, count_of_object);
 
-    object_for_sort.Sort(array_prices, num_object);
+    cout<< "Catalog: " << endl;
 
-    cout<<"Catalog:\n" << endl;
+    Comparing(array_prices, sneakers, count_of_object);
 
-    comparing(array_prices, sneakers, num_object);
+    cout << "The catalog has been sorted by price" << endl;
+    cout << "----------------------------------------------------------" << endl;
+    cout << "If you want to see the top, enter 1, else program will be closed" << endl;
+    int choice = 0;
+    cin >> choice;
 
-    cout<<"The catalog has been sorted by price"<<endl;
-    cout <<"----------------------------------------------------------"<< endl;
-    cout<<"If you want to see the top, enter 1, else program will be closed"<<endl;
-    int choise = 0;
-    cin >> choise;
-
-    if (choise == 1)
+    if (choice == 1)
     {
-        cout <<"----------------------------------------------------------"<< endl;
-        cout <<"Top sales :\n"<<endl;
-        int *array_sales = new int[num_object];
-        for(int temporary_index = 0; temporary_index < num_object; temporary_index++)
+        cout << "----------------------------------------------------------" << endl;
+        cout << "Top sales : " << endl;
+
+        int *array_sales = new int[count_of_object];
+
+        for(int i = 0; i < count_of_object; i++)
         {
-            array_sales[temporary_index] = sneakers[temporary_index].quantity_sales;
+            array_sales[i] = sneakers[i].quantity_sales;
         }
 
-        object_for_sort.Sort(array_sales,num_object);
+        Object_for_sorting_array.Sort_array(array_sales,count_of_object);
 
-        comparing(array_sales, sneakers, num_object);
+        Comparing(array_sales, sneakers, count_of_object);
 
         delete[] array_sales;
     }else
     {
-        cout<<"----------End----------"<<endl;
+        cout << "----------End----------" << endl;
         return 0;
     }
 
